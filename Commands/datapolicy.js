85a3b680d62d3a7ebc6af24c2704a1ec15b9bf39
Nathaniel Tseng <nathanieltseng@gmail.com>
2020-12-02 11:43:12 -0800

    1.0 Improvement - Message Embed for datapolicy command #9

--- Commands/datapolicy.js
+++ Commands/datapolicy.js
@@ -7,18 +7,26 @@ var command = new Command(["DataPolicy"], // aliases
 	["`@HorizonsBot DataPolicy`"]); // texts (must match number of headings)
 
 command.help = (avatarURL) => {
-	return dataPolicyBuilder();
+	return dataPolicyBuilder(avatarURL);
 }
 
 command.execute = (receivedMessage, state) => {
 	// Command specifications go here
-	receivedMessage.author.send(dataPolicyBuilder())
+	receivedMessage.author.send(dataPolicyBuilder(receivedMessage.client.user.displayAvatarURL()))
 		.catch(console.error);
 }
 
 module.exports = command;
 
-function dataPolicyBuilder() {
-	return `As of version 1.0.0, this bot collects the following user data:\n\t- **User Discord IDs** for managing channel visibility per user\n\n\
-	As of version 1.0.0, Imaginary Horizons Productions does not use the above user data for anything.`
+function dataPolicyBuilder(footerURL) {
+	return new MessageEmbed().setColor(`6b81eb`)
+		.setAuthor("Imaginary Horizons Productions", `https://cdn.discordapp.com/icons/353575133157392385/c78041f52e8d6af98fb16b8eb55b849a.png `, `https://discord.gg/bcE3Syu `)
+		.setURL(`https://github.com/Imaginary-Horizons-Productions/DirectoryBot `)
+		.setTitle(`HorizonsBot Data Policy`)
+		.setThumbnail('https://cdn.discordapp.com/attachments/545684759276421120/782019073562378298/shaking-hands.png')
+		.setDescription(`If you leave Imaginary Horizons, your data will be deleted.`)
+		.addField(`Data Collected (version 1.0)`, `HorizonsBot does not collect user data at this time.`)
+		.addField(`Data Usage (version 1.0)`, `Imaginary Horizons does not use uncollected user data at this time.`)
+		.setFooter(``, footerURL)
+		.setTimestamp();
 }
