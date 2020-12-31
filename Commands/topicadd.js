a603bb9c6c21bccb0eb477d0c1c33a7596e03c60
Nathaniel Tseng <nathanieltseng@gmail.com>
2020-12-31 12:57:02 -0800

    1.1 Feature - pinlist command (#24)

--- Commands/topicadd.js
+++ Commands/topicadd.js
@@ -1,6 +1,6 @@
 const Command = require('../Classes/Command.js');
 const { Permissions } = require('discord.js');
-const { topicList, roleIDs, saveObject } = require('../helpers.js');
+const { topicList, roleIDs, listMessages, topicListBuilder, saveObject } = require('../helpers.js');
 
 var command = new Command(["TopicAdd"], // aliases
 	"Sets up an opt-in text channel for the given topic", // description
@@ -30,6 +30,12 @@ command.execute = (receivedMessage, state) => {
 			]
 		}).then(channel => {
 			topicList.push(channel.id);
+			let messageData = listMessages.topics;
+			if (messageData) {
+				let channelManager = receivedMessage.guild.channels;
+				let message = channelManager.resolve(messageData.channelID).messages.resolve(messageData.messageID);
+				message.edit(topicListBuilder(channelManager));
+			}
 			saveObject(topicList, "topicList.json");
 		})
 	} else {
--- helpers.js
+++ helpers.js
@@ -1,14 +1,38 @@
 const fs = require('fs');
+const { MessageEmbed } = require('discord.js');
 exports.serverID = "353575133157392385";
 exports.roleIDs = require('./roleIDs.json');
 
 exports.moderatorIDs = require('./data/moderatorIDs.json');
 // messageID: channelID
 exports.embedsList = require('./data/embedsList.json');
+// type: {messageID: number, channelID: number}
+exports.listMessages = require('./data/listMessageIDs.json');
 // [channelID]
 exports.topicList = require('./data/topicList.json');
 // channelID: Campaign
 exports.campaignList = require('./data/campaignList.json');
+
+exports.topicListBuilder = function (channelManager) {
+    let embed = new MessageEmbed()
+        .setTitle("Topic Channels")
+        .setTimestamp();
+
+    let loops = Math.min(exports.topicList.length, 25);
+    if (loops > 0) {
+        for (let i = 0; i < loops; i += 1) {
+            let id = exports.topicList[i];
+            let channel = channelManager.resolve(id);
+            embed.addField(`${channel.name}`, `Channel ID: ${channel.id}`);
+        }
+    }
+
+    return embed;
+}
+
+exports.campaignListBuilder = function () {
+
+}
 
 exports.saveObject = function (object, fileName) {
     var filePath = `./`;
