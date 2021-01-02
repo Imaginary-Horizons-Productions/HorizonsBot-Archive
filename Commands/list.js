202a721e557763f4b2086949fb7095dd35adede3
Nathaniel Tseng <nathanieltseng@gmail.com>
2021-01-01 16:34:23 -0800

    1.1 Feature - list command (#14) - fixed crash when not using pinlist without arguments

---
+++ Commands/list.js
@@ -0,0 +1,30 @@
+const Command = require('../Classes/Command.js');
+const { serverID, topicListBuilder, campaignListBuilder } = require('../helpers.js');
+
+var command = new Command(["List"], // aliases
+	"Provide a list of topic or campaign channels", // description
+	"None", // requirements
+	["Example - replace ( ) with your settings"], // headings
+	[`@HorizonsBot List (topic or campaign)`]); // texts (must match number of headings)
+
+command.execute = (receivedMessage, state) => {
+	// Determine if user mentioned a topic or campaign, then provide appropriate permissions
+	if (state.messageArray.length > 0) {
+		let listType = state.messageArray[0].toLowerCase();
+		let guild = receivedMessage.client.guilds.resolve(serverID);
+		if (listType == "topic" || listType == "topics") {
+			receivedMessage.author.send(topicListBuilder(guild.channels))
+				.catch(console.log);
+		} else if (listType == "campaign" || listType == "campaigns") {
+			//TODO impliment for campaigns
+		} else {
+			receivedMessage.author.send(`Please specify either \`topic\` or \`campaign\` for the type of list.`)
+				.catch(console.log);
+		}
+	} else {
+		receivedMessage.author.send(`Please specify either \`topic\` or \`campaign\` for the type of list.`)
+			.catch(console.log);
+	}
+}
+
+module.exports = command;
--- Commands/pinlist.js
+++ Commands/pinlist.js
@@ -11,24 +11,29 @@ command.execute = (receivedMessage, state) => {
 	// Command specifications go here
 	if (moderatorIDs.includes(receivedMessage.author.id)) {
 		if (receivedMessage.guild) {
-			let listType = state.messageArray[0].toLowerCase();
-			if (listType == "topic" || listType == "topics") {
-				receivedMessage.channel.send(topicListBuilder(receivedMessage.guild.channels)).then(message => {
-					listMessages.topics = {
-						"messageID": message.id,
-						"channelID": message.channel.id
-					}
-					message.pin();
-				}).catch(console.log);
-			} else if (listType == "campaign" || listType == "campaigns") {
-				//TODO impliment for campaigns
-			} else {
-				receivedMessage.author.send(`Please specify either \`topic\` or \`campaign\` for the type of list to pin.`)
-					.catch(console.log);
-			}	
+			if (state.messageArray.length > 0) {
+				let listType = state.messageArray[0].toLowerCase();
+				if (listType == "topic" || listType == "topics") {
+					receivedMessage.channel.send(topicListBuilder(receivedMessage.guild.channels)).then(message => {
+						listMessages.topics = {
+							"messageID": message.id,
+							"channelID": message.channel.id
+						}
+						message.pin();
+					}).catch(console.log);
+				} else if (listType == "campaign" || listType == "campaigns") {
+					//TODO impliment for campaigns
+				} else {
+					receivedMessage.author.send(`Please specify either \`topic\` or \`campaign\` for the type of list to pin.`)
+						.catch(console.log);
+				}
+			}
+		} else {
+			receivedMessage.author.send(`Pinning topic or campaign lists is restricted to Moderators.`)
+				.catch(console.log);
 		}
 	} else {
-		receivedMessage.author.send(`Pinning topic or campaign lists is restricted to Moderators.`)
+		receivedMessage.author.send(`Please specify either \`topic\` or \`campaign\` for the type of list.`)
 			.catch(console.log);
 	}
 }
