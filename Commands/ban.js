ecb8343d1c3917497574901d51acfcbaab9601aa
Nathaniel Tseng <nathanieltseng@gmail.com>
2021-01-14 11:49:42 -0800

    1.1 Feature - ban command (#34)

--- Commands/ban.js
+++ Commands/ban.js
@@ -1,19 +1,20 @@
 const Command = require('../Classes/Command.js');
 const { moderatorIDs, topicList, campaignList } = require('../helpers.js');
 
-var command = new Command(["Kick"], // aliases
-	"Kick a user from an opt-in channel or TRPG campaign", // description
+var command = new Command(["Ban"], // aliases
+	"Bans the mentioned users from an opt-in channel or TRPG campaign", // description
 	"Moderator, must be used from an opt-in channel or campaign channel", // requirements
 	["Example - replace ( ) with your settings"], // headings
-	["`@HorizonsBot Kick (user)`"]); // texts (must match number of headings)
+	["`@HorizonsBot Ban (users)`"]); // texts (must match number of headings)
 
 command.execute = (receivedMessage, state) => {
 	// Remove visibility of receiving channel from mentioned user
 	if (moderatorIDs.includes(receivedMessage.author.id)) {
 		let leavableChannelIDs = topicList.concat(Object.keys(campaignList));
 		if (leavableChannelIDs.includes(receivedMessage.channel.id)) {
-			receivedMessage.channel.permissionOverwrites.get(receivedMessage.mentions.users.keyArray().filter(id => id != receivedMessage.client.user.id)[0]).delete(`kicked by ${receivedMessage.member.name}`)
-				.catch(console.error);
+			receivedMessage.mentions.users.keyArray().filter(id => id != receivedMessage.client.user.id).forEach(id => {
+				receivedMessage.channel.createOverwrite(id, { VIEW_CHANNEL: false }, `Banned by ${receivedMessage.author.tag}`);
+			})
 		} else {
 			receivedMessage.author.send(`Please use the \`leave\` command from a topic or campaign channel.`)
 				.catch(console.error);
--- Commands/join.js
+++ Commands/join.js
@@ -11,19 +11,26 @@ command.execute = (receivedMessage, state) => {
 	// Determine if user mentioned a topic or campaign, then provide appropriate permissions
 	let channelID = state.messageArray[0]; // can't use mention because users can't mention channels that are invisible to them (even by constructing the mention manually)
 	if (channelID) {
-		if (topicList.includes(channelID)) {
-			let guild = receivedMessage.client.guilds.resolve(getGuildID(receivedMessage.client.user.id));
-			let channel = guild.channels.resolve(channelID);
-			channel.createOverwrite(receivedMessage.author, {
-				"VIEW_CHANNEL": true
-			}).then(() => {
-				channel.send(`Welcome to ${channel.name}, ${receivedMessage.author}!`)
-					.catch(console.log);
-			})
-		} else if (Object.keys(campaignList).includes(channelID)) {
-			//TODO implement with campaign tracking
+		let guild = receivedMessage.client.guilds.resolve(getGuildID(receivedMessage.client.user.id));
+		let channel = guild.channels.resolve(channelID);
+		let permissionOverwrite = channel.permissionOverwrites.get(receivedMessage.author.id);
+		if (!permissionOverwrite || !permissionOverwrite.deny.has("VIEW_CHANNEL", false)) {
+			if (topicList.includes(channelID)) {
+				channel.createOverwrite(receivedMessage.author, {
+					"VIEW_CHANNEL": true
+				}).then(() => {
+					channel.send(`Welcome to ${channel.name}, ${receivedMessage.author}!`)
+						.catch(console.log);
+				})
+			} else if (Object.keys(campaignList).includes(channelID)) {
+				//TODO implement with campaign tracking
+			} else {
+				receivedMessage.author.send(`The ID you provided does not seem to be associated with a topic or campaign channel.`)
+					.catch(console.error);
+			}
 		} else {
-			receivedMessage.author.send(`The ID you provided does not seem to be associated with a topic or campaign channel.`)
+			receivedMessage.author.send(`You are currently banned from ${channel.name}. Speak to a Moderator if you believe this is in error.`)
+				.catch(console.error);
 		}
 	} else {
 		receivedMessage.author.send(`Please mention the ID of a channel to join.`)
