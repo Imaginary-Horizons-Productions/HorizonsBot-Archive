5ce167a6ed64f3ff0c9e91d229af045952bb6ca3
Nathaniel Tseng <nathanieltseng@gmail.com>
2021-02-02 10:38:44 -0800

    1.3 Feature - campaignsetsystem command (#22)

---
+++ Commands/campaignsetsystem.js
@@ -0,0 +1,31 @@
+const Command = require('../Classes/Command.js');
+const { isModerator, getCampaignList, setCampaignList, updateList } = require("../helpers.js");
+
+var command = new Command(["CampaignSetSystem"], // aliases
+	"Sets the system for a campaign", // description
+	"Moderator or Campaign Host, use from campaign text channel", // requirements
+	["__Example__ - replace ( ) with your settings"], // headings
+	["`@HorizonsBot CampaignSetSystem (system)`"]); // texts (must match number of headings)
+
+command.execute = (receivedMessage, state) => {
+	// Set the decription for the receiving campaign channel
+	let campaigns = getCampaignList();
+	if (isModerator(receivedMessage.author.id) || (campaigns[receivedMessage.channel.id] && receivedMessage.author.id == campaigns[receivedMessage.channel.id].hostID)) {
+		let system = state.messageArray.join(' ');
+		if (system) {
+			campaigns[receivedMessage.channel.id].system = system;
+			setCampaignList(campaigns);
+			receivedMessage.author.send(`${campaigns[receivedMessage.channel.id].name}'s system has been set as ${system}.`)
+				.catch(console.error);
+			updateList(receivedMessage.guild.channels, "campaigns");
+		} else {
+			receivedMessage.author.send(`Please provide the system for the campaign.`)
+				.catch(console.error);
+		}
+	} else {
+		receivedMessage.author.send(`Setting a campaign system is restricted to the host of that campaign or Moderators from that campaign channel.`)
+			.catch(console.error);
+	}
+}
+
+module.exports = command;
