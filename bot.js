7fddc728e9e3ac3c026ff3450a22b1d2fef2f7e3
Nathaniel Tseng <nathanieltseng@gmail.com>
2021-02-23 09:46:04 -0800

    1.4 Improvement - prevented stream notifications from being processed as commands

--- bot.js
+++ bot.js
@@ -55,6 +55,7 @@ client.on('message', receivedMessage => {
     // Publish stream notifications
     if (receivedMessage.author.id == "368105370532577280" && receivedMessage.channel.id == "572197893201592328" && receivedMessage.content.includes("Arcane_ish")) {
         receivedMessage.crosspost();
+        return;
     }
 
     // Process commands
