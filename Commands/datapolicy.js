b89055046551aa463d4e6ff8725ae8a07a95036b
Nathaniel Tseng <nathanieltseng@gmail.com>
2021-08-09 15:56:02 -0700

    1.6 Feature - clubsettimeslot and datapolicy for discord.js v13 (#85)

--- Commands/datapolicy.js
+++ Commands/datapolicy.js
@@ -24,9 +24,9 @@ function dataPolicyBuilder(footerURL) {
 		.setAuthor("Imaginary Horizons Productions", `https://cdn.discordapp.com/icons/353575133157392385/c78041f52e8d6af98fb16b8eb55b849a.png `)
 		.setTitle(`HorizonsBot Data Policy`)
 		.setThumbnail('https://cdn.discordapp.com/attachments/545684759276421120/782019073562378298/shaking-hands.png')
-		.setDescription(`If you leave Imaginary Horizons, your data will be deleted. This message was last updated with version 1.6.0 of HorizonsBot.`)
+		.setDescription(`If you leave Imaginary Horizons, your data will be deleted.`)
 		.addField(`Data Collected`, `HorizonsBot stores user submitted topics, petitions, and club details.`)
 		.addField(`Data Usage`, `Imaginary Horizons does not use any user data at this time.`)
-		.setFooter(``, footerURL)
+		.setFooter(`Updated: HorizonsBot version 1.6.0`, footerURL)
 		.setTimestamp();
 }
