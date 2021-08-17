cde56fe76a2f5c4fe6c81b4f4e42725fc3091182
Nathaniel Tseng <nathanieltseng@gmail.com>
2021-08-17 15:32:41 -0700

    1.7 Feature - rollwithmax as slash command (#87)

--- Commands/rollWithMax.js
+++ Commands/rollWithMax.js
@@ -7,6 +7,9 @@ var command = new Command(["RollWithMax"],
 	["Example - replace ( ) with your settings"],
 	["`@HorizonsBot RollWithMax (dice in #d# format) [label]`"]);
 
+	command.data.addStringOption(option => option.setName("dice").setDescription("The number and type of dice, use #d# format").setRequired(true))
+	.addStringOption(option => option.setName("label").setDescription("Text label for the roll").setRequired(false));
+
 command.execute = (receivedMessage, state) => {
 	if (state.messageArray.length > 0) {
 		var rollResult = getRollString(state.messageArray.join(' '), true, true);
@@ -15,6 +18,17 @@ command.execute = (receivedMessage, state) => {
 		receivedMessage.author.send('Please provide the number/type of dice you want to role in #d# format.')
 			.catch(console.error);
 	}
+};
+
+command.executeInteraction = (interaction) => {
+	// Roll dice, and to the max
+	var rollInput = interaction.options.getString('dice');
+	var label = interaction.options.getString('label');
+	if (label) {
+		rollInput = rollInput.concat(` ${label}`);
+	}
+	var rollResult = getRollString(rollInput, true, true);
+	interaction.reply(`Roll Result:\n\`${rollResult}\``);
 };
 
 module.exports = command;
