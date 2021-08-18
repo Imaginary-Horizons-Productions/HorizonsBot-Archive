e462a2828b89bd00ab427e78d959fc9985985c05
Nathaniel Tseng <nathanieltseng@gmail.com>
2021-08-17 19:51:39 -0700

    1.7 Feature - pinlist as slash command (#87)

--- Commands/pinlist.js
+++ Commands/pinlist.js
@@ -7,27 +7,46 @@ var command = new Command(["PinList"], // aliases
 	["Example - replace ( ) with your settings"], // headings
 	[`@HorizonsBot PinList (topic or club)`]); // texts (must match number of headings)
 
+command.data.addStringOption(option => option.setName("listtype").setDescription(`"topic" or "club"`).setRequired(true));
+
 command.execute = (receivedMessage, state) => {
 	// Command specifications go here
 	if (isModerator(receivedMessage.author.id)) {
-		if (receivedMessage.guild) {
-			if (state.messageArray.length > 0) {
-				let listType = state.messageArray[0].toLowerCase();
-				if (listType == "topic" || listType == "topics") {
-					pinTopicsList(receivedMessage.guild.channels, receivedMessage.channel);
-				} else if (listType == "club" || listType == "clubs") {
-					pinClubsList(receivedMessage.guild.channels, receivedMessage.channel);
-				} else {
-					receivedMessage.author.send(`Please specify either \`topic\` or \`club\` for the type of list to pin.`)
-						.catch(console.log);
-				}
+		if (state.messageArray.length > 0) {
+			let listType = state.messageArray[0].toLowerCase();
+			if (listType == "topic" || listType == "topics") {
+				pinTopicsList(receivedMessage.guild.channels, receivedMessage.channel);
+			} else if (listType == "club" || listType == "clubs") {
+				pinClubsList(receivedMessage.guild.channels, receivedMessage.channel);
+			} else {
+				receivedMessage.author.send(`Please specify either \`topic\` or \`club\` for the type of list to pin.`)
+					.catch(console.log);
 			}
+		}
+	} else {
+		receivedMessage.author.send(`Pinning topic or club lists is restricted to Moderators.`)
+			.catch(console.log);
+	}
+}
+
+command.executeInteraction = (interaction) => {
+	// Pin the list message for topics or clubs to the receiving channel
+	if (isModerator(interaction.user.id)) {
+		let listType = interaction.options.getString("listtype").toLowerCase();
+		if (listType == "topic" || listType == "topics") {
+			pinTopicsList(interaction.guild.channels, interaction.channel);
+			interaction.reply({ content: "Pinning the topic list succeded.", ephemeral: true })
+				.catch(console.error);
+		} else if (listType == "club" || listType == "clubs") {
+			pinClubsList(interaction.guild.channels, interaction.channel);
+			interaction.reply({ content: "Pinning the club list succeded.", ephemeral: true })
+				.catch(console.error);
 		} else {
-			receivedMessage.author.send(`Pinning topic or club lists is restricted to Moderators.`)
+			interaction.reply({ content: `Please specify either \`topic\` or \`club\` for the type of list to pin.`, ephemeral: true })
 				.catch(console.log);
 		}
 	} else {
-		receivedMessage.author.send(`Please specify either \`topic\` or \`club\` for the type of list.`)
+		interaction.reply({ content: `Pinning topic or club lists is restricted to Moderators.`, ephemeral: true })
 			.catch(console.log);
 	}
 }
