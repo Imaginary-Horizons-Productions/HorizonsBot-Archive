d1f920d8964072c35750b6073188a2b48544724f
Nathaniel Tseng <nathanieltseng@gmail.com>
2021-08-22 22:01:38 -0700

    1.7 Feature - delete as slash command (#87)

--- Commands/delete.js
+++ Commands/delete.js
@@ -7,6 +7,8 @@ var command = new Command(["Delete"], // aliases
 	["Example - replace [ ] with settings (optional)"], // headings
 	["`@HorizonsBot Delete [delay in hours]`"]); // texts (must match number of headings)
 
+command.data.addIntegerOption(option => option.setName("delay").setDescription("Number of hours to delay deleting the channel").setRequired(false));
+
 command.execute = (receivedMessage, state) => {
 	// Delete a topic or club channel, or set it to be deleted on a delay
 	if (isModerator(receivedMessage.author.id)) {
@@ -29,6 +31,32 @@ command.execute = (receivedMessage, state) => {
 		}
 	} else {
 		receivedMessage.author.send("Deleting channels is restricted to Moderators.")
+			.catch(console.error);
+	}
+}
+
+command.executeInteraction = (interaction) => {
+	// Delete a topic or club channel, or set it to be deleted on a delay
+	if (isModerator(interaction.user.id)) {
+		if (getManagedChannels().includes(interaction.channel.id)) {
+			let delay = parseFloat(interaction.options.getInteger("delay"));
+			if (!isNaN(delay)) {
+				interaction.reply(`This channel has been scheduled to be deleted in ${delay} hour(s).`)
+					.catch(console.error);
+				setTimeout(() => {
+					interaction.channel.delete()
+						.catch(console.error);
+				}, delay * 3600000)
+			} else {
+				interaction.channel.delete()
+					.catch(console.error);
+			}
+		} else {
+			interaction.reply({ content: "The delete command can only be used on topic or club channels.", ephemeral: true })
+				.catch(console.error);
+		}
+	} else {
+		interaction.reply({ content: "Deleting channels is restricted to Moderators.", ephemeral: true })
 			.catch(console.error);
 	}
 }
