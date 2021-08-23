f5b2f60bbd9fda8fff31fd8e08e72c53e20d2b4b
Nathaniel Tseng <nathanieltseng@gmail.com>
2021-08-22 22:27:28 -0700

    1.7 Feature - embedcreate as slash command (#87)

--- Commands/embedcreate.js
+++ Commands/embedcreate.js
@@ -27,4 +27,24 @@ command.execute = (receivedMessage, state) => {
 	}
 }
 
+command.executeInteraction = (interaction) => {
+	// Create a new MessageEmbed
+	if (interaction.guild) {
+		if (isModerator(interaction.user.id)) {
+			let embed = new MessageEmbed().setFooter("Custom Embed").setTimestamp();
+			interaction.channel.send({ embeds: [embed] }).then(message => {
+				customEmbeds[message.id] = message.channel.id;
+				saveObject(customEmbeds, "embedsList.json");
+				interaction.reply({ content: "Here's your new embed.", ephemeral: true });
+			}).catch(console.error);
+		} else {
+			interaction.reply({ content: `You must be a Moderator to use the \`${state.command}\` command.`, ephemeral: true })
+				.catch(console.error);
+		}
+	} else {
+		interaction.reply({ content: `The \`${state.command}\` command must be used from a server channel.`, ephemeral: true })
+			.catch(console.error);
+	}
+}
+
 module.exports = command;
