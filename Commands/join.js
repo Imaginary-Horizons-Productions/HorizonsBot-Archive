f5f450de70cd2a37673fb3c5310ab0c40810b74e
Nathaniel Tseng <nathanieltseng@gmail.com>
2021-08-22 21:20:09 -0700

    1.7 Feature - join as slash command (#87)

--- Commands/join.js
+++ Commands/join.js
@@ -7,6 +7,9 @@ var command = new Command(["Join"], // aliases
 	["Example - replace ( ) with your settings"], // headings
 	[`@HorizonsBot Join (channel names or IDs)`]); // texts (must match number of headings)
 
+command.data.addStringOption(option => option.setName("channel").setDescription("The name (or ID) of the topic or club to join").setRequired(true));
+// can't use channel mention because users can't mention channels that are invisible to them (even by constructing the mention manually)
+
 command.execute = (receivedMessage, state) => {
 	// Determine if user mentioned a topic or club, then provide appropriate permissions
 	if (state.messageArray.length > 0) {
@@ -28,6 +31,25 @@ command.execute = (receivedMessage, state) => {
 		receivedMessage.author.send(`Please provide the name or ID of a channel to join.`)
 			.catch(console.error);
 	}
+}
+
+command.executeInteraction = (interaction) => {
+	// Determine if user mentioned a topic or club, then provide appropriate permissions
+	interaction.client.guilds.fetch(guildID).then(guild => {
+		var channelName = interaction.options.getString("channel");
+		if (isNaN(parseInt(channelName))) {
+			let channelID = findTopicID(channelName.toLowerCase());
+			if (channelID) {
+				let channel = guild.channels.resolve(channelID);
+				joinChannel(channel, interaction.user);
+				interaction.reply({ content: "Channel joined!", ephemeral: true });
+			}
+		} else {
+			let channel = guild.channels.resolve(channelName);
+			joinChannel(channel, interaction.user);
+			interaction.reply({ content: "Channel joined!", ephemeral: true });
+		}
+	});
 }
 
 module.exports = command;
