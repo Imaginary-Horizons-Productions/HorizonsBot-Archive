100c4fa0fd8e2d832d58fb0130cb21a2072a453a
Nathaniel Tseng <nathanieltseng@gmail.com>
2021-08-22 21:04:09 -0700

    1.7 Feature and Improvement - merged kick and ban commands - kick and ban as slash command (#87)

--- Commands/kick.js
+++ Commands/kick.js
@@ -1,11 +1,14 @@
 const Command = require('../Classes/Command.js');
 const { isModerator, getManagedChannels, getClubs, updateClub } = require('../helpers.js');
 
-var command = new Command(["Kick"], // aliases
+var command = new Command(["Kick", "Ban"], // aliases
 	"Remove mentioned users from a topic or club channel", // description
 	"Moderator, must be used from an opt-in channel or club channel", // requirements
 	["Example - replace ( ) with your settings"], // headings
 	["`@HorizonsBot Kick (users)`"]); // texts (must match number of headings)
+
+command.data.addUserOption(option => option.setName("target").setDescription("The user to remove from the topic or club").setRequired(true))
+	.addBooleanOption(option => option.setName("ban").setDescription("Prevent user from rejoining?").setRequired(false));
 
 command.execute = (receivedMessage, state) => {
 	// Remove visibility of receiving channel from mentioned user
@@ -25,6 +28,35 @@ command.execute = (receivedMessage, state) => {
 		}
 	} else {
 		receivedMessage.author.send(`Kicking users from text channels is restricted to Moderators.`)
+			.catch(console.error);
+	}
+}
+
+command.executeInteraction = (interaction) => {
+	// Remove visibility of receiving channel from mentioned user
+	if (isModerator(interaction.user.id)) {
+		if (getManagedChannels().includes(interaction.channel.id)) {
+			var user = interaction.options.getUser("target");
+			var club = getClubs()[interaction.channel.id];
+			if (club) {
+				club.userIDs = club.userIDs.filter(memberId => memberId != user.id);
+				updateClub(club, interaction.guild.chanels);
+			}
+			if (interaction.options.getBoolean("ban")) {
+				interaction.channel.permissionOverwrites.create(user.id, { VIEW_CHANNEL: false }, `Banned by ${interaction.user}`);
+				interaction.reply(`${user} has been banned from this channel.`)
+					.catch(console.error);
+			} else {
+				interaction.channel.permissionOverwrites.delete(user, `Kicked by ${interaction.user}`);
+				interaction.reply(`${user} has been kicked from this channel.`)
+					.catch(console.error);
+			}
+		} else {
+			interaction.reply(`Please use the \`kick\` command from a topic or club channel.`)
+				.catch(console.error);
+		}
+	} else {
+		interaction.reply(`Kicking users from text channels is restricted to Moderators.`)
 			.catch(console.error);
 	}
 }
