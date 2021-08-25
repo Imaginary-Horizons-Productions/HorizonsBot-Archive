5bb552a576483972d4a157555426ddf1f22e17c0
Nathaniel Tseng <nathanieltseng@gmail.com>
2021-08-25 16:37:36 -0700

    1.7 Feature - embedsetcolor as slash command (#87)

--- Commands/embedsetcolor.js
+++ Commands/embedsetcolor.js
@@ -7,6 +7,9 @@ var command = new Command(["EmbedSetColor", "EmbedSetColour"], // aliases
 	["Example - replace ( ) with your settings"], // headings
 	["`@HorizonsBot EmbedSetColor (message ID) (color hexcode)`"]); // texts (must match number of headings)
 
+command.data.addStringOption(option => option.setName("messageid").setDescription("The ID of the embed's message").setRequired(true))
+	.addStringOption(option => option.setName("color").setDescription("The hexcode of the color").setRequired(true));
+
 command.execute = (receivedMessage, state) => {
 	// Set the color for the given embed
 	if (isModerator(receivedMessage.author.id)) {
@@ -30,6 +33,34 @@ command.execute = (receivedMessage, state) => {
 		}
 	} else {
 		receivedMessage.author.send(`You must be a Moderator to use the \`${state.command}\` command.`)
+			.catch(console.error);
+	}
+}
+
+command.executeInteraction = (interaction) => {
+	// Set the color for the given embed
+	if (isModerator(interaction.user.id)) {
+		let messageID = interaction.options.getString("messageid");
+		if (customEmbeds[messageID]) {
+			let colorCode = interaction.options.getString("color");
+			if (colorCode.match(/^#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$/)) {
+				interaction.client.guilds.fetch(guildID).then(guild => {
+					guild.channels.resolve(customEmbeds[messageID]).messages.fetch(messageID).then(message => {
+						let embed = message.embeds[0].setColor(colorCode).setTimestamp();
+						message.edit({ embeds: [embed] });
+						interaction.reply({ content: `The embed's color has been updated. Link: ${message.url}`, ephemeral: true });
+					})
+				})
+			} else {
+				interaction.reply(`Please provide color in hex code format (#000000).`)
+					.catch(console.error);
+			}
+		} else {
+			interaction.reply(`The embed you provided for a \`${interaction.commandName}\` command could not be found.`)
+				.catch(console.error);
+		}
+	} else {
+		interaction.reply(`You must be a Moderator to use the \`${interaction.commandName}\` command.`)
 			.catch(console.error);
 	}
 }
