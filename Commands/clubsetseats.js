d11bce4df48b9ddaecf2e55d0301c7fb18dfdf0b
Nathaniel Tseng <nathanieltseng@gmail.com>
2021-08-26 11:29:40 -0700

    1.7 Feature - clubsetseats as slash command (#87)

--- Commands/clubsetseats.js
+++ Commands/clubsetseats.js
@@ -7,6 +7,8 @@ var command = new Command(["ClubSetSeats", "CampaignSetSeats"], // aliases
 	["Example - replace ( ) with your settings"], // headings
 	["`@HorizonsBot ClubSetSeats (number)`"]); // texts (must match number of headings)
 
+command.data.addIntegerOption(option => option.setName("seats").setDescription("The maximum number of members for the club").setRequired(true));
+
 command.execute = (receivedMessage, state) => {
 	// Set the decription for the receiving club channel
 	let club = getClubs()[receivedMessage.channel.id];
@@ -28,6 +30,25 @@ command.execute = (receivedMessage, state) => {
 		}
 	} else {
 		receivedMessage.author.send(`Please set club settings from the club channel.`)
+			.catch(console.error);
+	}
+}
+
+command.executeInteraction = (interaction) => {
+	// Set the decription for the receiving club channel
+	let club = getClubs()[interaction.channel.id];
+	if (club) {
+		if (isModerator(interaction.user.id) || interaction.user.id == club.hostID) {
+			club.seats = interaction.options.getInteger("seats");
+			updateClub(club, interaction.guild.channels);
+			interaction.reply({ content: `${club.title}'s max member count has been set as ${club.seats}.`, ephemeral: true })
+				.catch(console.error);
+		} else {
+			interaction.reply({ content: `Setting a club max member count is restricted to the host of that club or Moderators.`, ephemeral: true })
+				.catch(console.error);
+		}
+	} else {
+		interaction.reply({ content: `Please set club settings from the club channel.`, ephemeral: true })
 			.catch(console.error);
 	}
 }
