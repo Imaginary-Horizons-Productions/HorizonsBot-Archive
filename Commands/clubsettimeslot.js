ec9780da456ba9c634608638870e646113a7caa8
Nathaniel Tseng <nathanieltseng@gmail.com>
2021-08-26 11:35:11 -0700

    1.7 Feature - clubsettimeslot as slash command (#87)

--- Commands/clubsettimeslot.js
+++ Commands/clubsettimeslot.js
@@ -7,6 +7,8 @@ var command = new Command(["ClubSetTimeSlot", "CampaignSetTimeSlot"], // aliases
 	["Example - replace ( ) with your settings"], // headings
 	["`@HorizonsBot ClubSetTimeSlot (time slot)`"]); // texts (must match number of headings)
 
+command.data.addStringOption(option => option.setName("timeslot").setDescription("The text to set for the club timeslot").setRequired(true));
+
 command.execute = (receivedMessage, state) => {
 	// Set the decription for the receiving club channel
 	let club = getClubs()[receivedMessage.channel.id];
@@ -28,6 +30,25 @@ command.execute = (receivedMessage, state) => {
 		}
 	} else {
 		receivedMessage.author.send(`Please set club settings from the club channel.`)
+			.catch(console.error);
+	}
+}
+
+command.executeInteraction = (interaction) => {
+	// Set the decription for the receiving club channel
+	let club = getClubs()[interaction.channel.id];
+	if (club) {
+		if (isModerator(interaction.user.id) || (club && interaction.user.id == club.hostID)) {
+			club.timeslot = interaction.options.getString("timeslot");
+			updateClub(club, interaction.guild.channels);
+			interaction.reply({ content: `${club.title}'s time slot has been set as ${club.timeslot}.`, ephemeral: true })
+				.catch(console.error);
+		} else {
+			interaction.reply({ content: `Setting a club time slot is restricted to the host of that club or Moderators from that club channel.`, ephemeral: true })
+				.catch(console.error);
+		}
+	} else {
+		interaction.reply({ content: `Please set club settings from the club channel.`, ephemeral: true })
 			.catch(console.error);
 	}
 }
