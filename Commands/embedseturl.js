c15adcdb50adda5cde30a74b488aae18a7730145
Nathaniel Tseng <nathanieltseng@gmail.com>
2021-08-25 17:59:37 -0700

    1.7 Feature - embedseturl as slash command (#87)

--- Commands/embedseturl.js
+++ Commands/embedseturl.js
@@ -7,6 +7,9 @@ var command = new Command(["EmbedSetURL"], // aliases
 	["Example - replace ( ) with your settings"], // headings
 	["`@HorizonsBot EmbedSetURL (message ID) (url)`"]); // texts (must match number of headings)
 
+command.data.addStringOption(option => option.setName("messageid").setDescription("The ID of the embed's message").setRequired(true))
+	.addStringOption(option => option.setName("url").setDescription("The url to go to when clicking the title field").setRequired(true));
+
 command.execute = (receivedMessage, state) => {
 	// Set the title url for the given embed
 	if (isModerator(receivedMessage.author.id)) {
@@ -30,6 +33,34 @@ command.execute = (receivedMessage, state) => {
 		}
 	} else {
 		receivedMessage.author.send(`You must be a Moderator to use the \`${state.command}\` command.`)
+			.catch(console.error);
+	}
+}
+
+command.executeInteraction = (interaction) => {
+	// Set the title url for the given embed
+	if (isModerator(interaction.user.id)) {
+		let messageID = interaction.options.getString("messageid");
+		if (customEmbeds[messageID]) {
+			let url = interaction.options.getString("url");
+			if (url) {
+				interaction.client.guilds.fetch(guildID).then(guild => {
+					guild.channels.resolve(customEmbeds[messageID]).messages.fetch(messageID).then(message => {
+						let embed = message.embeds[0].setURL(url).setTimestamp();
+						message.edit({ embeds: [embed] });
+						interaction.reply({ content: `The embed's url has been updated. Link: ${message.url}`, ephemeral: true });
+					}).catch(console.error);
+				})
+			} else {
+				interaction.reply({ content: `Your url for a \`${interaction.commandName}\` command could not be parsed.`, ephemeral: true })
+					.catch(console.error);
+			}
+		} else {
+			interaction.reply({ content: `The embed you provided for a \`${interaction.commandName}\` command could not be found.`, ephemeral: true })
+				.catch(console.error);
+		}
+	} else {
+		interaction.reply({ content: `You must be a Moderator to use the \`${interaction.commandName}\` command.`, ephemeral: true })
 			.catch(console.error);
 	}
 }
