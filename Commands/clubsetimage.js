2950b25d4adf15f65c770238b37729f2f89c971b
Nathaniel Tseng <nathanieltseng@gmail.com>
2021-08-26 11:24:42 -0700

    1.7 Feature - clubsetimage as slash command (#87)

--- Commands/clubsetimage.js
+++ Commands/clubsetimage.js
@@ -2,10 +2,12 @@ const Command = require('../Classes/Command.js');
 const { isModerator, getClubs, updateClub } = require("../helpers.js");
 
 var command = new Command(["ClubSetImage", "CampaignSetImage"], // aliases
-	"Set a club's image url, from text or attachments", // description
+	"Set or clear a club's image url", // description
 	"Moderator or Club Host, use from club channel", // requirements
 	["Example - replace [ ] with settings (optional)"], // headings
 	["`@HorizonsBot ClubSetImage [url]`"]); // texts (must match number of headings)
+
+command.data.addStringOption(option => option.setName("url").setDescription("The url to the image to set for the club").setRequired(false));
 
 command.execute = (receivedMessage, state) => {
 	// Set the decription for the receiving club channel
@@ -31,6 +33,32 @@ command.execute = (receivedMessage, state) => {
 		}
 	} else {
 		receivedMessage.author.send(`Please set club settings from the club channel.`)
+			.catch(console.error);
+	}
+}
+
+command.executeInteraction = (interaction) => {
+	// Set the decription for the receiving club channel
+	let club = getClubs()[interaction.channel.id];
+	if (club) {
+		if (isModerator(interaction.user.id) || (club && interaction.user.id == club.hostID)) {
+			let url = interaction.options.getString("url");
+			if (url) {
+				club.imageURL = url;
+				interaction.reply({ content: `${club.title}'s image has been set.`, ephemeral: true })
+					.catch(console.error);
+			} else {
+				club.imageURL = "";
+				interaction.reply({ content: `${club.title}'s image has been cleared.`, ephemeral: true })
+					.catch(console.error);
+			}
+			updateClub(club, interaction.guild.channels);
+		} else {
+			interaction.reply({ content: `Setting a club image is restricted to the host of that club or Moderators from that club channel.`, ephemeral: true })
+				.catch(console.error);
+		}
+	} else {
+		interaction.reply({ content: `Please set club settings from the club channel.`, ephemeral: true })
 			.catch(console.error);
 	}
 }
