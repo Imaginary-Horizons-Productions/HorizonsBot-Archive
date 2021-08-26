01a62918c0bd1fbc6641ea286609824bd5fc30e4
Nathaniel Tseng <nathanieltseng@gmail.com>
2021-08-26 11:17:33 -0700

    1.7 Feature - clubsetgame as slash command (#87)

--- Commands/clubsetgame.js
+++ Commands/clubsetgame.js
@@ -7,6 +7,8 @@ var command = new Command(["ClubSetGame", "CampaignSetGame", "CampaignSetSystem"
 	["Example - replace ( ) with your settings"], // headings
 	["`@HorizonsBot ClubSetSystem (system)`"]); // texts (must match number of headings)
 
+command.data.addStringOption(option => option.setName("game").setDescription("The text to set as the club game").setRequired(true));
+
 command.execute = (receivedMessage, state) => {
 	// Set the decription for the receiving club channel
 	let club = getClubs()[receivedMessage.channel.id];
@@ -28,6 +30,25 @@ command.execute = (receivedMessage, state) => {
 		}
 	} else {
 		receivedMessage.author.send(`Please set club settings from the club channel.`)
+			.catch(console.error);
+	}
+}
+
+command.executeInteraction = (interaction) => {
+	// Set the decription for the receiving club channel
+	let club = getClubs()[interaction.channel.id];
+	if (club) {
+		if (isModerator(interaction.user.id) || interaction.user.id == club.hostID) {
+			club.system = interaction.options.getString("game");
+			updateClub(club, interaction.guild.channels);
+			interaction.reply({ content: `${club.title}'s game has been set as ${club.system}.`, ephemeral: true })
+				.catch(console.error);
+		} else {
+			interaction.reply({ content: `Setting a club game is restricted to the host of that club or Moderators.`, ephemeral: true })
+				.catch(console.error);
+		}
+	} else {
+		interaction.reply({ content: `Please set club settings from the club channel.`, ephemeral: true })
 			.catch(console.error);
 	}
 }
