a27f933210588c0808040287941b11ff6fc5b19d
Nathaniel Tseng <nathanieltseng@gmail.com>
2021-08-26 13:16:54 -0700

    1.7 Feature - embedsetmessage command (#91)

--- Commands/CommandsList.js
+++ Commands/CommandsList.js
@@ -6,7 +6,7 @@ exports.commandSets = [
 	new CommandSet("HorizonsBot Commands", "Here are all of HorizonsBot's commands. Check their details to see what the usage requirements are!",
 		['about.js', 'commands.js', 'support.js', 'datapolicy.js', "list.js", "join.js", "leave.js", "petition.js", "kick.js", "delete.js", "pinlist.js", "roll.js"]),
 	new CommandSet("Custom Message Embeds", "To get a message's id, enable developer mode (User Settings > Appearance > Developer Mode), then right-click the message and select \"Copy ID\".",
-		['embedcreate.js', 'embedabandon.js', 'embedsetauthor.js', 'embedsettitle.js', 'embedseturl.js', 'embedsetcolor.js', 'embedsetdescription.js', 'embedsetthumbnail.js', 'embedaddfield.js', 'embedsplicefields.js', 'embedsetimage.js']),
+		['embedcreate.js', 'embedabandon.js', 'embedsetauthor.js', 'embedsettitle.js', 'embedseturl.js', 'embedsetcolor.js', 'embedsetdescription.js', 'embedsetthumbnail.js', 'embedaddfield.js', 'embedsplicefields.js', 'embedsetimage.js', "embedsetmessage.js"]),
 	new CommandSet("Topic Commands", "Commands for managing opt-in topic text channels.",
 		["topicinvite.js", "topicadd.js", "topicveto.js"]),
 	new CommandSet("Club Commands", "Commands for managing club text and voice channels.",
---
+++ Commands/embedsetmessage.js
@@ -0,0 +1,30 @@
+const Command = require('../Classes/Command.js');
+const { guildID, customEmbeds, isModerator } = require('../helpers.js');
+
+var command = new Command("embed-set-message", "Assign a custom embed's message content");
+
+command.data.addStringOption(option => option.setName("messageid").setDescription("The ID of the embed's message").setRequired(true))
+	.addStringOption(option => option.setName("text").setDescription("The text to put in the message (above the embed)").setRequired(true));
+
+command.execute = (interaction) => {
+	// Set the message for the given embed
+	if (isModerator(interaction.user.id)) {
+		let messageID = interaction.options.getString("messageid");
+		if (customEmbeds[messageID]) {
+			interaction.client.guilds.fetch(guildID).then(guild => {
+				guild.channels.resolve(customEmbeds[messageID]).messages.fetch(messageID).then(message => {
+					message.edit({ content: interaction.options.getString("text") });
+					interaction.reply({ content: `The embed's message has been updated. Link: ${message.url}`, ephemeral: true })
+				}).catch(console.error);
+			})
+		} else {
+			interaction.reply({ content: `The embed you provided for a \`${interaction.commandName}\` command could not be found.`, ephemeral: true })
+				.catch(console.error);
+		}
+	} else {
+		interaction.reply({ content: `You must be a Moderator to use the \`${interaction.commandName}\` command.`, ephemeral: true })
+			.catch(console.error);
+	}
+}
+
+module.exports = command;
