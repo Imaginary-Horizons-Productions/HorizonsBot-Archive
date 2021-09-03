6da936239cbbd9883077abd2bf65c04a64a45f39
Nathaniel Tseng <nathanieltseng@gmail.com>
2021-09-03 15:54:09 -0700

    1.9 Documentation - updated data policy

--- Commands/datapolicy.js
+++ Commands/datapolicy.js
@@ -16,7 +16,7 @@ function dataPolicyBuilder(footerURL) {
 		.setAuthor("Imaginary Horizons Productions", `https://cdn.discordapp.com/icons/353575133157392385/c78041f52e8d6af98fb16b8eb55b849a.png `)
 		.setTitle(`HorizonsBot Data Policy`)
 		.setThumbnail('https://cdn.discordapp.com/attachments/545684759276421120/782019073562378298/shaking-hands.png')
-		.setDescription(`If you leave Imaginary Horizons, your data will be deleted. Your account creation date may be checked to verify against spam bots.`)
+		.setDescription(`If you leave Imaginary Horizons, your data will be deleted.`)
 		.addField(`Data Collected`, `HorizonsBot stores user submitted topics, petitions, and club details.`)
 		.addField(`Data Usage`, `Imaginary Horizons does not use any user data at this time.`)
 		.setFooter(`Updated: HorizonsBot version 1.8.0`, footerURL)
