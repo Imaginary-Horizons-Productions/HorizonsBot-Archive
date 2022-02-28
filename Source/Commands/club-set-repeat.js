a5de8d906a27b2ce1b33507f850ce3b6f66006e4
Nathaniel Tseng <nathanieltseng@gmail.com>
2022-02-27 19:56:46 -0800

    v1.16.0 Fix (#202) - re-enable customizing club reminders

--- Source/Commands/club-set-repeat.js
+++ Source/Commands/club-set-repeat.js
@@ -1,15 +1,16 @@
 const Command = require('../../Classes/Command.js');
 
 let options = [
-	{ type: "Integer", name: "count", description: "The number of units between meetings", required: true, choices: {} },
+	{ type: "Integer", name: "count", description: "The units of time between meetings", required: true, choices: {} },
 	{
-		type: "String", name: "time-unit", description: "The amount of time each unit represents", required: true, choices: {
+		type: "String", name: "time-unit", description: "The unit of time", required: true, choices: {
 			weeks: "w",
 			days: "d"
 		}
-	}
+	},
+	{ type: "String", name: "reminder-text", description: "The text of the reminder", required: false, choices: {} }
 ];
-module.exports = new Command("club-set-repeat", "Set how frequently club meetings repeat, this will set the next meetings automatically", options);
+module.exports = new Command("club-set-repeat", "Set how frequently to send reminders about club meetings", options);
 
 let getClubs, updateClub, updateClubDetails;
 module.exports.initialize = function (helpers) {
@@ -24,10 +25,23 @@ module.exports.execute = (interaction) => {
 		if (count > -1) {
 			club.timeslot.periodCount = count;
 			let units = interaction.options.getString("time-unit");
+			let message = interaction.options.getString("reminder-text");
+			if (message !== undefined && message !== null) {
+				club.timeslot.message = message;
+				interaction.reply({ content: `The reminder message for this club has been set to the following:\n\t${message}`, ephemeral: true });
+			} else {
+				club.timeslot.message = "";
+				interaction.reply({ content: `The reminder message for this club has been cleared.`, ephemeral: true });
+			}
 			club.timeslot.periodUnits = units;
 			updateClub(club, interaction.guild.channels);
 			updateClubDetails(club, interaction.channel);
-			interaction.reply(count !== 0 ? `This club has been set to repeat meetings every ${count} ${units === "w" ? "week" : "day"}(s).` : "Repeating meetings have been canceled for this club.");
+			if (count > 0) {
+				interaction.channel.send(`This club has been set to repeat meetings every ${count} ${units === "w" ? "week" : "day"}(s).`);
+			} else {
+				//TODO #204 cancel scheduled meeting
+				interaction.channel.send("Repeating meetings have been canceled for this club.");
+			}
 		} else {
 			interaction.reply({ content: "Please choose a positive non-zero number for count.", ephemeral: true });
 		}
