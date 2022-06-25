73824b22ee86f6ed684149fe60083f57e31c253c
Nathaniel Tseng <nathanieltseng@gmail.com>
2022-06-25 11:47:05 -0700

    v1.17.1 Documentation (#250) - added /roles, /rules, and /press-kit - extracted slash command embeds into json files

--- Source/Commands/_commandDictionary.js
+++ Source/Commands/_commandDictionary.js
@@ -1,11 +1,10 @@
 const CommandSet = require('../../Classes/CommandSet.js');
 
-// Total commands (new CommandSet when exceeding 25): 15
 exports.commandSets = [
 	new CommandSet("HorizonsBot Commands", "The general use commands. Required permissions are listed in (parenthesis) at the beginning of the description.",
-		['about.js', 'commands.js', 'support.js', 'data-policy.js', "list.js", "join.js", "leave.js", "roll.js", "timestamp.js", "at-channel.js", "version.js"]),
-	new CommandSet("Custom Message Embeds", "To get a message's id, enable developer mode (User Settings > Appearance > Developer Mode), then right-click the message and select \"Copy ID\".",
-		['embed-create.js', 'embed-abandon.js', 'embed-edit.js']),
+		['about.js', 'commands.js', 'data-policy.js', "list.js", "join.js", "leave.js", "roll.js", "timestamp.js", "at-channel.js", "version.js"]),
+	new CommandSet("Server Informantion", "To get a message's id, enable developer mode (User Settings > Appearance > Developer Mode), then right-click the message and select \"Copy ID\".",
+		['rules.js', 'roles.js', 'support.js', 'press-kit.js']),
 	new CommandSet("Topic Commands", "Commands for managing topics.",
 		["petition.js", "topic-invite.js", "topic-add.js", "petition-check.js", "petition-veto.js"]),
 	new CommandSet("Club Commands", "Commands for managing clubs.",
